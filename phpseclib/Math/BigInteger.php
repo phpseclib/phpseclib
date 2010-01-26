43044941193317da3e5aa4f301b9995e7b7105ed
Jim Wigginton <terrafrost@php.net>
2010-01-26 23:57:27 +0000

    - removed more debug code
    git-svn-id: http://phpseclib.svn.sourceforge.net/svnroot/phpseclib/trunk@80 21d32557-59b3-4da0-833f-c5933fad653e

--- phpseclib/Math/BigInteger.php
+++ phpseclib/Math/BigInteger.php
@@ -67,7 +67,7 @@
  * @author     Jim Wigginton <terrafrost@php.net>
  * @copyright  MMVI Jim Wigginton
  * @license    http://www.gnu.org/licenses/lgpl.txt
- * @version    $Id: BigInteger.php,v 1.26 2010-01-23 17:36:49 terrafrost Exp $
+ * @version    $Id: BigInteger.php,v 1.27 2010-01-26 23:57:27 terrafrost Exp $
  * @link       http://pear.php.net/package/Math_BigInteger
  */
 
@@ -3368,9 +3368,6 @@ class Math_BigInteger {
         if ( !count($value) ) {
             return $result;
         }
-//zzzzzzzzzzzzz
-//if ($this->bitmask) {
-//print_r($this->bitmask);exit;}
 
         $value = $this->_trim($value);
 
