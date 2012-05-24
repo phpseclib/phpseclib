d7079754264a31acf6bce90a87ff4f0defa02098
Andreas Fischer <bantu@phpbb.com>
2012-05-24 02:09:54 +0200

    [feature/phpunit] Add phpunit configuration and bootstrapping.

---
+++ tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Bootstrapping File for phpseclib Test Suite
+ *
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ */
+
+// Set up include path accordingly. This is especially required because some
+// class files of phpseclib require() other dependencies.
+set_include_path(implode(PATH_SEPARATOR, array(
+	dirname(__FILE__) . '/../phpseclib/',
+	get_include_path(),
+)));
