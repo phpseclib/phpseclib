1f1ece2d00426c0308c67d88e8c5a656af843550
Katelyn Schiesser <slowbro@chatnets.net>
2012-11-14 09:50:33 -0800

    fix Crypt_TripleDES not properly turncating keys at 24 bytes

--- phpseclib/Crypt/TripleDES.php
+++ phpseclib/Crypt/TripleDES.php
@@ -344,7 +344,7 @@ class Crypt_TripleDES {
     {
         $length = strlen($key);
         if ($length > 8) {
-            $key = str_pad($key, 24, chr(0));
+             $key = str_pad(substr($key, 0, 24), 24, chr(0));
             // if $key is between 64 and 128-bits, use the first 64-bits as the last, per this:
             // http://php.net/function.mcrypt-encrypt#47973
             //$key = $length <= 16 ? substr_replace($key, substr($key, 0, 8), 16) : substr($key, 0, 24);
