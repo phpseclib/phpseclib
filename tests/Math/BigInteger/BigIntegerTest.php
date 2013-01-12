f897e36e9695296a05dce114bad4fb61cbdbe429
Andreas Fischer <bantu@phpbb.com>
2013-01-12 02:18:41 +0100

    [constant-trickery] Turn existing BigIntegerTest into an abstract TestCase.

---
+++ tests/Math/BigInteger/BigIntegerTest.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author     Andreas Fischer <bantu@phpbb.com>
+ * @copyright  MMXIII Andreas Fischer
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ */
+
+class Math_BigInteger_BigIntegerTest extends Math_BigInteger_TestCase
+{
+
+}
