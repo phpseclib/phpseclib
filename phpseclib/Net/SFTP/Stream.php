5cd0191eddaaf544f310e87d4752ee73749d543a
terrafrost <terrafrost@phpbb.com>
2013-04-01 10:53:55 -0500

    SFTP_Stream: update includes

--- phpseclib/Net/SFTP/Stream.php
+++ phpseclib/Net/SFTP/Stream.php
@@ -37,8 +37,8 @@
 /**
  * Include Net_SSH2
  */
-if (!class_exists('Net_SSH2')) {
-    require_once('../Net/SSH2.php');
+if (!class_exists('Net_SFTP')) {
+    require_once('Net/SFTP.php');
 }
 
 /**
