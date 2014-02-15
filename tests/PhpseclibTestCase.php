ef528ea8793480aad2a931c1c5ea9dc7f661134d
Andreas Fischer <bantu@phpbb.com>
2014-02-15 19:57:49 +0100

    Apply coding guidelines (with exceptions) to the tests directory.

--- tests/PhpseclibTestCase.php
+++ tests/PhpseclibTestCase.php
@@ -1,74 +1,64 @@
 <?php
 /**
- * @author     Andreas Fischer <bantu@phpbb.com>
- * @copyright  MMXIII Andreas Fischer
- * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @author    Andreas Fischer <bantu@phpbb.com>
+ * @copyright MMXIII Andreas Fischer
+ * @license   http://www.opensource.org/licenses/mit-license.html  MIT License
  */
 
 abstract class PhpseclibTestCase extends PHPUnit_Framework_TestCase
 {
-	/**
-	* @param string $constant
-	* @param mixed $expected
-	*
-	* @return null
-	*/
-	static protected function ensureConstant($constant, $expected)
-	{
-		if (defined($constant))
-		{
-			$value = constant($constant);
+    /**
+    * @param string $constant
+    * @param mixed $expected
+    *
+    * @return null
+    */
+    static protected function ensureConstant($constant, $expected)
+    {
+        if (defined($constant)) {
+            $value = constant($constant);
 
-			if ($value !== $expected)
-			{
-				if (function_exists('runkit_constant_redefine'))
-				{
-					if (!runkit_constant_redefine($constant, $expected))
-					{
-						self::markTestSkipped(sprintf(
-							"Failed to redefine constant %s to %s",
-							$constant,
-							$expected
-						));
-					}
-				}
-				else
-				{
-					self::markTestSkipped(sprintf(
-						"Skipping test because constant %s is %s instead of %s",
-						$constant,
-						$value,
-						$expected
-					));
-				}
-			}
-		}
-		else
-		{
-			define($constant, $expected);
-		}
-	}
+            if ($value !== $expected) {
+                if (function_exists('runkit_constant_redefine')) {
+                    if (!runkit_constant_redefine($constant, $expected)) {
+                        self::markTestSkipped(sprintf(
+                            "Failed to redefine constant %s to %s",
+                            $constant,
+                            $expected
+                        ));
+                    }
+                } else {
+                    self::markTestSkipped(sprintf(
+                        "Skipping test because constant %s is %s instead of %s",
+                        $constant,
+                        $value,
+                        $expected
+                    ));
+                }
+            }
+        } else {
+            define($constant, $expected);
+        }
+    }
 
-	/**
-	* @param string $filename
-	*
-	* @return null
-	*/
-	static protected function reRequireFile($filename)
-	{
-		if (function_exists('runkit_import'))
-		{
-			$result = runkit_import(
-				$filename,
-				RUNKIT_IMPORT_FUNCTIONS |
-				RUNKIT_IMPORT_CLASS_METHODS |
-				RUNKIT_IMPORT_OVERRIDE
-			);
+    /**
+    * @param string $filename
+    *
+    * @return null
+    */
+    static protected function reRequireFile($filename)
+    {
+        if (function_exists('runkit_import')) {
+            $result = runkit_import(
+                $filename,
+                RUNKIT_IMPORT_FUNCTIONS |
+                RUNKIT_IMPORT_CLASS_METHODS |
+                RUNKIT_IMPORT_OVERRIDE
+            );
 
-			if (!$result)
-			{
-				self::markTestSkipped("Failed to reimport file $filename");
-			}
-		}
-	}
+            if (!$result) {
+                self::markTestSkipped("Failed to reimport file $filename");
+            }
+        }
+    }
 }
