c391f52221914c4a01106441b31873db7ef9ae8c
Andreas Fischer <bantu@phpbb.com>
2014-04-10 20:00:48 +0200

    Prevent segfault on PHP 5.3.3 by disabling GC.

--- travis/run-phpunit.sh
+++ travis/run-phpunit.sh
@@ -7,7 +7,14 @@ export PHPSECLIB_SSH_USERNAME='phpseclib'
 export PHPSECLIB_SSH_PASSWORD='EePoov8po1aethu2kied1ne0'
 export PHPSECLIB_SSH_HOME='/home/phpseclib'
 
+PHPUNIT_EXTRA_ARGS=''
+if [ "$TRAVIS_PHP_VERSION" = '5.3.3' ]
+then
+  PHPUNIT_EXTRA_ARGS="$PHPUNIT_EXTRA_ARGS -d zend.enable_gc=0"
+fi
+
 phpunit \
+  $PHPUNIT_EXTRA_ARGS \
   --verbose \
   --coverage-text \
   --coverage-html code_coverage/
