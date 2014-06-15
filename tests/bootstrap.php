952de03535902d9d34f9dc2e92f205a06eeba72d
John Slegers <slegersjohn@gmail.com>
2014-06-15 15:59:02 +0200

    Making main fork compatible with PSR-0 autoloading
    Adding support of support of PHP >= 5.3 namespaces, PSR-0 autoloading
    and a couple of other basic improvements.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,5 @@
 <?php
+define(LIBRARY_PATH, dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR .'vendors');
 /**
  * Bootstrapping File for phpseclib Test Suite
  *
@@ -7,37 +8,5 @@
 
 date_default_timezone_set('UTC');
 
-// Set up include path accordingly. This is especially required because some
-// class files of phpseclib require() other dependencies.
-set_include_path(implode(PATH_SEPARATOR, array(
-    dirname(__FILE__) . '/../phpseclib/',
-    dirname(__FILE__) . '/',
-    get_include_path(),
-)));
-
-function phpseclib_is_includable($suffix)
-{
-    foreach (explode(PATH_SEPARATOR, get_include_path()) as $prefix) {
-        $ds = substr($prefix, -1) == DIRECTORY_SEPARATOR ? '' : DIRECTORY_SEPARATOR;
-        $file = $prefix . $ds . $suffix;
-
-        if (file_exists($file)) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-function phpseclib_autoload($class)
-{
-    $file = str_replace('_', '/', $class) . '.php';
-
-    if (phpseclib_is_includable($file)) {
-        // @codingStandardsIgnoreStart
-        require $file;
-        // @codingStandardsIgnoreEnd
-    }
-}
-
-spl_autoload_register('phpseclib_autoload');
+require LIBRARY_PATH . DIRECTORY_SEPARATOR . 'Loader.php';
+Loader::init(array(LIBRARY_PATH));
