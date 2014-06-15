5601f7036d78303abd615973340d7b8a03ab3e47
John Slegers <slegersjohn@gmail.com>
2014-06-15 18:13:55 +0200

    Update TestCase.php

--- tests/Unit/Crypt/AES/TestCase.php
+++ tests/Unit/Crypt/AES/TestCase.php
@@ -5,8 +5,6 @@
  * @license   http://www.opensource.org/licenses/mit-license.html  MIT License
  */
 
-require_once 'Crypt/AES.php';
-
 abstract class Unit_Crypt_AES_TestCase extends PhpseclibTestCase
 {
     static public function setUpBeforeClass()
