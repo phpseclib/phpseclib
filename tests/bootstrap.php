44773fe946db823d945806e103fdea6f0af1759f
John Slegers <slegersjohn@gmail.com>
2014-06-15 17:58:40 +0200

    Fix autoloader issue
    Fix autoloader issue

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,4 @@
 <?php
-$LIBRARY_PATH = dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR .'vendors';
 /**
  * Bootstrapping File for phpseclib Test Suite
  *
@@ -8,5 +7,5 @@ $LIBRARY_PATH = dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPAR
 
 date_default_timezone_set('UTC');
 
-require $LIBRARY_PATH . DIRECTORY_SEPARATOR . 'Loader.php';
+require dirname(__FILE__) . DIRECTORY_SEPARATOR . 'Loader.php';
 Loader::init(array($LIBRARY_PATH));
