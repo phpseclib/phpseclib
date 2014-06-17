7a9c742822ac751ef4a4064456b080e35636046b
terrafrost <terrafrost@php.net>
2014-06-16 21:29:41 -0500

    Random: i wonder if this will break anything

--- phpseclib/Crypt/Random.php
+++ phpseclib/Crypt/Random.php
@@ -252,3 +252,32 @@ if (!function_exists('crypt_random_string')) {
         return substr($result, 0, $length);
     }
 }
+
+
+if (!function_exists('stream_resolve_include_path')) {
+    /**
+     * Resolve filename against the include path.
+     *
+     * stream_resolve_include_path was introduced in PHP 5.3.2. This is kinda a PHP_Compat layer for those not using that version.
+     *
+     * @param Integer $length
+     * @return String
+     * @access public
+     */
+    function stream_resolve_include_path($filename)
+    {
+        $paths = PATH_SEPARATOR == ':' ?
+            preg_split('#(?<!phar):#', get_include_path()) :
+            explode(PATH_SEPARATOR, get_include_path());
+        foreach ($paths as $prefix) {
+            $ds = substr($prefix, -1) == DIRECTORY_SEPARATOR ? '' : DIRECTORY_SEPARATOR;
+            $file = $prefix . $ds . $filename;
+
+            if (file_exists($file)) {
+                return $file;
+            }
+        }
+
+        return false;
+    }
+}
