f908040d202b46780ac1c870633f5ac5ed95e756
Andreas Fischer <bantu@phpbb.com>
2014-07-30 16:56:09 +0200

    Reenable SSH Functional Tests on Travis CI (PHP 5.3.3 and below).

--- tests/Functional/Net/SSH2Test.php
+++ tests/Functional/Net/SSH2Test.php
@@ -8,16 +8,6 @@
 
 class Functional_Net_SSH2Test extends PhpseclibFunctionalTestCase
 {
-    public function setUp()
-    {
-        if (getenv('TRAVIS') && version_compare(PHP_VERSION, '5.3.3', '<=')) {
-            $this->markTestIncomplete(
-                'This test hangs on Travis CI on PHP 5.3.3 and below.'
-            );
-        }
-        parent::setUp();
-    }
-
     public function testConstructor()
     {
         $ssh = new Net_SSH2($this->getEnv('SSH_HOSTNAME'));
