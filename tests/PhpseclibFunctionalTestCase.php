ca0971227ebc85506bc61e8ae8111e5484453ac9
terrafrost <terrafrost@php.net>
2014-08-01 10:28:47 -0500

    ...

--- tests/PhpseclibFunctionalTestCase.php
+++ tests/PhpseclibFunctionalTestCase.php
@@ -7,6 +7,15 @@
 
 abstract class PhpseclibFunctionalTestCase extends PhpseclibTestCase
 {
+    static protected function setUpBeforeClass()
+    {
+        self::ensureConstant('MATH_BIGINTEGER_MODE', MATH_BIGINTEGER_MODE_GMP);
+        self::ensureConstant('CRYPT_HASH_MODE', CRYPT_HASH_MODE_HASH);
+        self::reRequireFile('Math/BigInteger.php');
+        self::reRequireFile('Crypt/Hash.php');
+        parent::setUpBeforeClass();
+    }
+
     /**
     * @param string $variable
     * @param string|null $message
