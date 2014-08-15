0d6fbb9e300d405eff11cee3a6edb8a18e94c91b
terrafrost <terrafrost@php.net>
2014-08-15 09:23:55 -0500

    AES: don't do mcrypt unit tests if mcrypt extension is not available

--- tests/Unit/Crypt/AES/McryptTest.php
+++ tests/Unit/Crypt/AES/McryptTest.php
@@ -9,6 +9,10 @@ class Unit_Crypt_AES_McryptTest extends Unit_Crypt_AES_TestCase
 {
     static public function setUpBeforeClass()
     {
+        if (!extension_loaded('mcrypt')) {
+            self::markTestSkipped('mcrypt extension is not available.');
+        }
+
         parent::setUpBeforeClass();
 
         self::ensureConstant('CRYPT_AES_MODE', CRYPT_AES_MODE_MCRYPT);
