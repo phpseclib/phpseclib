4f3f0a5d277b3916ef21aa0564bc7d0607751a83
terrafrost <terrafrost@php.net>
2015-02-14 13:21:17 -0600

    one last update

--- tests/Unit/Net/SSH2Test.php
+++ tests/Unit/Net/SSH2Test.php
@@ -41,14 +41,9 @@ class Unit_Net_SSH2Test extends PhpseclibTestCase
         $identifier = $this->createSSHMock()->_generate_identifier();
         $this->assertStringStartsWith('SSH-2.0-phpseclib_0.3', $identifier);
 
-        if (extension_loaded('openssl')) {
-            $this->assertContains('openssl', $identifier);
-            $this->assertNotContains('mcrypt', $identifier);
-        } else if (extension_loaded('mcrypt')) {
-            $this->assertNotContains('openssl', $identifier);
+        if (extension_loaded('mcrypt')) {
             $this->assertContains('mcrypt', $identifier);
         } else {
-            $this->assertNotContains('openssl', $identifier);
             $this->assertNotContains('mcrypt', $identifier);
         }
 
