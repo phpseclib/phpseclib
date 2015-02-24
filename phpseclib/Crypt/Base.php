eb0055fc454a4672981639648889a0f7bb95cf3c
terrafrost <terrafrost@php.net>
2015-02-24 08:22:06 -0600

    Crypt/Base: engine wasn't being set correctly

--- phpseclib/Crypt/Base.php
+++ phpseclib/Crypt/Base.php
@@ -1232,7 +1232,7 @@ class Crypt_Base
         );
         foreach ($candidateEngines as $engine) {
             if ($this->isValidEngine($engine)) {
-                $this->engine = $this->preferredEngine;
+                $this->engine = $engine;
                 break;
             }
         }
