936a07025bf2ff1f9f7db9911f804193a7adde88
terrafrost <terrafrost@php.net>
2015-03-21 13:05:16 -0500

    Tests/AES: rm OpenSSL bit from code - save it for next PR

--- tests/Unit/Crypt/AES/TestCase.php
+++ tests/Unit/Crypt/AES/TestCase.php
@@ -16,9 +16,6 @@ abstract class Unit_Crypt_AES_TestCase extends PhpseclibTestCase
         if ($aes->getEngine() != $this->engine) {
             $engine = 'internal';
             switch ($this->engine) {
-                case CRYPT_ENGINE_OPENSSL:
-                    $engine = 'OpenSSL';
-                    break;
                 case CRYPT_ENGINE_MCRYPT:
                     $engine = 'mcrypt';
             }
