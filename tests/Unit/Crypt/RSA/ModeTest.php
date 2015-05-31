1316fd45f44d44f7e3b414d804f5210737f199f8
terrafrost <terrafrost@php.net>
2015-05-31 09:17:53 -0500

    Tests/RSA: use correct constant

--- tests/Unit/Crypt/RSA/ModeTest.php
+++ tests/Unit/Crypt/RSA/ModeTest.php
@@ -31,7 +31,7 @@ U9VQQSQzY1oZMVX8i1m5WUTLPz2yLJIBQVdXqhMCQBGoiuSoSjafUhV7i1cEGpb88h5NBYZzWXGZ
         $rsa->loadKey($privatekey);
         $rsa->loadKey($rsa->getPublicKey());
 
-        $rsa->setEncryptionMode(CRYPT_RSA_ENCRYPTION_NONE);
+        $rsa->setEncryptionMode(RSA::ENCRYPTION_NONE);
         $expected = '105b92f59a87a8ad4da52c128b8c99491790ef5a54770119e0819060032fb9e772ed6772828329567f3d7e9472154c1530f8156ba7fd732f52ca1c06' .
             '5a3f5ed8a96c442e4662e0464c97f133aed31262170201993085a589565d67cc9e727e0d087e3b225c8965203b271e38a499c92fc0d6502297eca712' .
             '4d04bd467f6f1e7c';
