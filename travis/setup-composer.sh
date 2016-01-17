46698b38c1b3923dc30ff8b7effc10740b85dbea
terrafrost <terrafrost@php.net>
2016-01-17 08:08:09 -0600

    TravisCI: dont use openssl with composer for PHP 5.3.3

--- travis/setup-composer.sh
+++ travis/setup-composer.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-composer self-update --no-interaction
+if [ "$TRAVIS_PHP_VERSION" == "5.3.3" ]
+then
+    # openssl is disabled on travis ci on 5.3.3:
+    # https://docs.travis-ci.com/user/languages/php#PHP-installation
+    composer self-update --no-interaction --disable-tls
+else
+    composer self-update --no-interaction
+fi
 composer install --no-interaction
