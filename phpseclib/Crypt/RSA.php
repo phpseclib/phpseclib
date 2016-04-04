ac97ce5d69888f785b1db37d1c4d4dc34e942aff
terrafrost <terrafrost@php.net>
2016-04-03 21:34:11 -0500

    RSA: backport macdabby's changes

--- phpseclib/Crypt/RSA.php
+++ phpseclib/Crypt/RSA.php
@@ -1786,7 +1786,7 @@ class Crypt_RSA
     function setPrivateKey($key = false, $type = false)
     {
         if ($key === false && !empty($this->publicExponent)) {
-            unset($this->publicExponent);
+            $this->publicExponent = false;
             return true;
         }
 
@@ -1794,7 +1794,7 @@ class Crypt_RSA
         if (!$rsa->loadKey($key, $type)) {
             return false;
         }
-        unset($rsa->publicExponent);
+        $rsa->publicExponent = false;
 
         // don't overwrite the old key if the new key is invalid
         $this->loadKey($rsa);
