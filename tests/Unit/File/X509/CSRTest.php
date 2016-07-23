f339c5adcc6405481f78a942b264c62ccf997fad
terrafrost <terrafrost@php.net>
2016-07-23 18:44:20 -0500

    Tests/X509: update test to work with master branch

--- tests/Unit/File/X509/CSRTest.php
+++ tests/Unit/File/X509/CSRTest.php
@@ -101,7 +101,7 @@ draiRBZruwMPwPIP
         $rsa = new RSA();
         $x509 = new X509();
 
-        $rsa->loadKey('-----BEGIN RSA PRIVATE KEY-----
+        $rsa->load('-----BEGIN RSA PRIVATE KEY-----
 MIICXAIBAAKBgQCqGKukO1De7zhZj6+H0qtjTkVxwTCpvKe4eCZ0FPqri0cb2JZfXJ/DgYSF6vUp
 wmJG8wVQZKjeGcjDOL5UlsuusFncCzWBQ7RKNUSesmQRMSGkVb1/3j+skZ6UtW+5u09lHNsj6tQ5
 1s1SPrCBkedbNf0Tp0GbMJDyR4e9T04ZZwIDAQABAoGAFijko56+qGyN8M0RVyaRAXz++xTqHBLh
