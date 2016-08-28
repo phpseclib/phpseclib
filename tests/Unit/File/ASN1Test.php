6dc50cc5c967a74aa653f6362de5723b0fc0517d
terrafrost <terrafrost@php.net>
2016-08-28 10:45:31 -0500

    ASN1: update unit test for 2.0 branch

--- tests/Unit/File/ASN1Test.php
+++ tests/Unit/File/ASN1Test.php
@@ -295,7 +295,7 @@ class Unit_File_ASN1Test extends PhpseclibTestCase
      */
     public function testInfiniteLoop()
     {
-        $asn1 = new File_ASN1();
+        $asn1 = new ASN1();
         $data = base64_decode('MD6gJQYKKwYBBAGCNxQCA6AXDBVvZmZpY2VAY2VydGRpZ2l0YWwucm+BFW9mZmljZUBjZXJ0ZGlnaXRhbC5ybw==');
         $asn1->decodeBER($data);
     }
