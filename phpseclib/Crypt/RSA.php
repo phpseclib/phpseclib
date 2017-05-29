a451dd69f85dd3c8c1c9b6542a6c0393a9f6b0b9
terrafrost <terrafrost@php.net>
2017-05-29 06:31:54 -0500

    RSA: reset variables if bad key was loaded

--- phpseclib/Crypt/RSA.php
+++ phpseclib/Crypt/RSA.php
@@ -1661,6 +1661,15 @@ class Crypt_RSA
         }
 
         if ($components === false) {
+            $this->comment = null;
+            $this->modulus = null;
+            $this->k = null;
+            $this->exponent = null;
+            $this->primes = null;
+            $this->exponents = null;
+            $this->coefficients = null;
+            $this->publicExponent = null;
+
             return false;
         }
 
