ce2bfd984eaef4e07178be7abe6c9d726e0c2ddc
terrafrost <terrafrost@php.net>
2017-07-16 12:09:45 -0500

    Tests: fix CS errors

--- tests/Unit/Crypt/TripleDESTest.php
+++ tests/Unit/Crypt/TripleDESTest.php
@@ -190,11 +190,11 @@ class Unit_Crypt_TripleDESTest extends PhpseclibTestCase
     // test special case lambda function error
     public function testCorrectSelfUseInLambda()
     {
-        $td = new TripleDES( TripleDES::MODE_ECB );
-        $td->setPreferredEngine( TripleDES::ENGINE_INTERNAL );
-        for ( $i = 0; $i < 20; $i++ ) {
-            $td->setKey( str_repeat( 'a', 20 ) . pack( 'V', mt_rand() ) );
-            $td->encrypt( str_repeat( 'a', 32 ) );
+        $td = new TripleDES(TripleDES::MODE_ECB);
+        $td->setPreferredEngine(TripleDES::ENGINE_INTERNAL);
+        for ($i = 0; $i < 20; $i++) {
+            $td->setKey(str_repeat('a', 20) . pack('V', mt_rand()));
+            $td->encrypt(str_repeat('a', 32));
         }
     }
 }
