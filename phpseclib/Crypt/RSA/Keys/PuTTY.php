a3b252150bdd16c959b9ec2ffc350ec92d97a838
Sokolovskyy Roman <Roman.Sokolovskyy@dixonscarphone.com>
2017-08-03 09:14:32 +0200

    Set of PHPDOC fixes

--- phpseclib/Crypt/RSA/Keys/PuTTY.php
+++ phpseclib/Crypt/RSA/Keys/PuTTY.php
@@ -50,7 +50,7 @@ abstract class PuTTY extends Progenitor
      * @access public
      * @param string $key
      * @param string $password optional
-     * @return array
+     * @return array|bool
      */
     public static function load($key, $password = '')
     {
