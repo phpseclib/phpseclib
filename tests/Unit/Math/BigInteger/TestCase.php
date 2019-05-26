50d6c8aafd00398c5bf118cd373d90aa1934a8b0
terrafrost <terrafrost@php.net>
2019-05-26 15:21:27 -0500

    Tests/BigInteger: test for new BigInteger('-0')

--- tests/Unit/Math/BigInteger/TestCase.php
+++ tests/Unit/Math/BigInteger/TestCase.php
@@ -465,5 +465,8 @@ abstract class Unit_Math_BigInteger_TestCase extends PhpseclibTestCase
     {
         $temp = $this->getInstance('00');
         $this->assertSame($temp->toString(), '0');
+
+        $temp = $this->getInstance('-0');
+        $this->assertSame($temp->toString(), '0');
     }
 }
