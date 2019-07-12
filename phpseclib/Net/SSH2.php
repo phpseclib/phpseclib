9015d987f7d9fbd9d307fce89504d91dc89a8dca
terrafrost <terrafrost@php.net>
2019-07-12 07:36:12 -0500

    only auto close the channel for exec() timeouts

--- phpseclib/Net/SSH2.php
+++ phpseclib/Net/SSH2.php
@@ -3815,7 +3815,9 @@ class Net_SSH2
                     // on windows this returns a "Warning: Invalid CRT parameters detected" error
                     if (!@stream_select($read, $write, $except, $sec, $usec) && !count($read)) {
                         $this->is_timeout = true;
-                        $this->_close_channel($client_channel);
+                        if ($client_channel == NET_SSH2_CHANNEL_EXEC && !$this->request_pty) {
+                            $this->_close_channel($client_channel);
+                        }
                         return true;
                     }
                     $elapsed = strtok(microtime(), ' ') + strtok('') - $start;
