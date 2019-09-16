e6b48d15fea702689b15f3329bfa82174b7eafea
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-09-16 13:21:36 +0200

    ErrorException: fclose() expects parameter 1 to be resource, null given
    Trace
    
    /vendor/phpseclib/phpseclib/phpseclib/Net/SSH2.php:4122
    /vendor/phpseclib/phpseclib/phpseclib/Net/SFTP.php:3167
    /vendor/phpseclib/phpseclib/phpseclib/Net/SSH2.php:3157
    /vendor/phpseclib/phpseclib/phpseclib/Net/SSH2.php:3173

--- phpseclib/Net/SSH2.php
+++ phpseclib/Net/SSH2.php
@@ -3154,7 +3154,10 @@ class SSH2
      */
     function disconnect()
     {
-        $this->_disconnect(NET_SSH2_DISCONNECT_BY_APPLICATION);
+        if($this->isConnected()){
+            $this->_disconnect(NET_SSH2_DISCONNECT_BY_APPLICATION);
+        }
+       
         if (isset($this->realtime_log_file) && is_resource($this->realtime_log_file)) {
             fclose($this->realtime_log_file);
         }
