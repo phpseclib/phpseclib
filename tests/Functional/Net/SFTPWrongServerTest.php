f19ffb8ede368b918456061de71636f83fdbd8b5
Vladmir Gaydamaka <vladimir.gaydamaka@gmail.com>
2019-09-17 14:23:39 +0300

    Simplified test

--- tests/Functional/Net/SFTPWrongServerTest.php
+++ tests/Functional/Net/SFTPWrongServerTest.php
@@ -1,35 +1,18 @@
 <?php
 
-/**
- * @author Vladimir Gaydamaka <vladimir.gaydamaka@gmail.com>
- * @copyright 20019 Vladimir Gaydamaka
- * @license   http://www.opensource.org/licenses/mit-license.html  MIT License
- */
-
 use phpseclib\Net\SFTP;
-use PHPUnit\Framework\Error\Warning;
+use PHPUnit\Framework\Error\Error;
+use PHPUnit\Framework\TestCase;
 
-class SFTPWrongServerTest extends Functional_Net_SFTPTestCase
+class SFTPWrongServerTest extends TestCase
 {
-    public function setUp()
-    {
-      parent::setUp();
-      $this->scratchDir = uniqid('phpseclib-sftp-scratch-');
-
-      $this->sftp = new SFTP($this->getEnv('SSH_WRONG_HOSTNAME'));
+  public function testLoginToInvalidServer()
+  {
+    try {
+      (new SFTP('dummy-server'))->login('username', 'password');
+      static::fail('The connection to the non-existent server must not happen.');
+    } catch (Error $e) {
+      static::assertSame('Cannot connect to dummy-server:22. Error 0. php_network_getaddresses: getaddrinfo failed: Name or service not known', $e->getMessage());
     }
-
-    public function testLogin()
-    {
-      try {
-        $this->sftp->login(
-          $this->getEnv('SSH_USERNAME'),
-          $this->getEnv('SSH_PASSWORD')
-        );
-        $this->fail('Cannot connect to ' . $this->getEnv('SSH_WRONG_HOSTNAME'));
-      }
-      catch (\PHPUnit\Framework\Error\Error $e) {
-        $this->assertSame('Cannot connect to ' . $this->getEnv('SSH_WRONG_HOSTNAME') .':22. Error 0. php_network_getaddresses: getaddrinfo failed: Name or service not known', $e->getMessage());
-      }
-    }
+  }
 }
