0dfacba3f95ee1180dcbcb26f69e97501034abd0
Vladmir Gaydamaka <vladimir.gaydamaka@gmail.com>
2019-09-17 14:25:39 +0300

    Added tab

--- tests/Functional/Net/SFTPWrongServerTest.php
+++ tests/Functional/Net/SFTPWrongServerTest.php
@@ -6,13 +6,13 @@ use PHPUnit\Framework\TestCase;
 
 class SFTPWrongServerTest extends TestCase
 {
-  public function testLoginToInvalidServer()
-  {
-    try {
-      (new SFTP('dummy-server'))->login('username', 'password');
-      static::fail('The connection to the non-existent server must not happen.');
-    } catch (Error $e) {
-      static::assertSame('Cannot connect to dummy-server:22. Error 0. php_network_getaddresses: getaddrinfo failed: Name or service not known', $e->getMessage());
+    public function testLoginToInvalidServer()
+    {
+        try {
+          (new SFTP('dummy-server'))->login('username', 'password');
+          static::fail('The connection to the non-existent server must not happen.');
+        } catch (Error $e) {
+          static::assertSame('Cannot connect to dummy-server:22. Error 0. php_network_getaddresses: getaddrinfo failed: Name or service not known', $e->getMessage());
+        }
     }
-  }
 }
