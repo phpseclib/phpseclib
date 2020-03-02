fc0832ae99b13ce34ceb45f1125927a5d632288c
terrafrost <terrafrost@gmail.com>
2020-03-02 10:18:30 -0600

    ASN1: fix for malformed ASN1 strings

--- phpseclib/File/ASN1.php
+++ phpseclib/File/ASN1.php
@@ -577,6 +577,10 @@ class File_ASN1
      */
     function asn1map($decoded, $mapping, $special = array())
     {
+        if (!is_array($decoded)) {
+            return false;
+        }
+
         if (isset($mapping['explicit']) && is_array($decoded['content'])) {
             $decoded = $decoded['content'][0];
         }
