f780640ecf726809314035936ce1114686de1f75
terrafrost <terrafrost@gmail.com>
2020-05-12 22:03:06 -0500

    move more common elements to StreamCipher class

--- phpseclib/Crypt/Common/StreamCipher.php
+++ phpseclib/Crypt/Common/StreamCipher.php
@@ -25,6 +25,28 @@ namespace phpseclib3\Crypt\Common;
 abstract class StreamCipher extends SymmetricKey
 {
     /**
+     * Block Length of the cipher
+     *
+     * Stream ciphers do not have a block size
+     *
+     * @see \phpseclib3\Crypt\Common\SymmetricKey::block_size
+     * @var int
+     * @access private
+     */
+    protected $block_size = 0;
+
+    /**
+     * Default Constructor.
+     *
+     * @see \phpseclib3\Crypt\Common\SymmetricKey::__construct()
+     * @return \phpseclib3\Crypt\Common\StreamCipher
+     */
+    public function __construct()
+    {
+        parent::__construct('stream');
+    }
+
+    /**
      * Stream ciphers not use an IV
      *
      * @access public
