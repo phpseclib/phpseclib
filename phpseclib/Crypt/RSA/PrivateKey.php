c1b70c21cbccc119ab4b6e212462301a1cedf431
terrafrost <terrafrost@gmail.com>
2021-01-16 09:59:45 -0600

    RSA: the salt length isn't saved for new keys

--- phpseclib/Crypt/RSA/PrivateKey.php
+++ phpseclib/Crypt/RSA/PrivateKey.php
@@ -512,7 +512,7 @@ class PrivateKey extends RSA implements Common\PrivateKey
                 $options+= [
                     'hash' => $this->hash->getHash(),
                     'MGFHash' => $this->mgfHash->getHash(),
-                    'saltLength' => $this->sLen
+                    'saltLength' => $this->getSaltLength()
                 ];
             } else {
                 throw new UnsupportedFormatException('The PSS format can only be used when the signature method has been explicitly set to PSS');
