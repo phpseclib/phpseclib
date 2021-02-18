e92c0a5914789d832f7553fbfb3f69694790b80f
tim_get_me <gtcntKM$%6>
2021-02-18 19:11:56 +0300

    Switch off the condition for mbstring

--- phpseclib/bootstrap.php
+++ phpseclib/bootstrap.php
@@ -9,12 +9,12 @@
  *
  * @license http://www.opensource.org/licenses/mit-license.html MIT License
  */
-if (extension_loaded('mbstring')) {
-    // 2 - MB_OVERLOAD_STRING
-    if (ini_get('mbstring.func_overload') & 2) {
-        throw new UnexpectedValueException(
-            'Overloading of string functions using mbstring.func_overload ' .
-            'is not supported by phpseclib.'
-        );
-    }
-}
+//if (extension_loaded('mbstring')) {
+//    // 2 - MB_OVERLOAD_STRING
+//    if (ini_get('mbstring.func_overload') & 2) {
+//        throw new UnexpectedValueException(
+//            'Overloading of string functions using mbstring.func_overload ' .
+//            'is not supported by phpseclib.'
+//        );
+//    }
+//}
