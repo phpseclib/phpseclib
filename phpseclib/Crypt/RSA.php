8af4280bdebf77b87b558510c925794380b84d16
terrafrost <terrafrost@gmail.com>
2021-04-03 17:55:12 -0500

    RSA: misc fixes for 'without NULL' PKCS1 signature validation

--- phpseclib/Crypt/RSA.php
+++ phpseclib/Crypt/RSA.php
@@ -3035,6 +3035,9 @@ class Crypt_RSA
                 break;
             case 'sha512':
                 $t = pack('H*', '304f300b06096086480165030402030440');
+                break;
+            default:
+                return false;
         }
         $t.= $h;
         $tLen = strlen($t);
