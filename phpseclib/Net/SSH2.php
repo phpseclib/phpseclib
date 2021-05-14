9a7cfafc8df7f16c81b1cff5b0ee6ae9d868be19
terrafrost <terrafrost@gmail.com>
2021-05-14 12:55:06 -0500

    SSH2: timeout would occasionally infinitely loop
    this was changed from !$this->curTimeout to $this->curTimeout <= 0
    in commit 96c4c3bc58

--- phpseclib/Net/SSH2.php
+++ phpseclib/Net/SSH2.php
@@ -3351,7 +3351,7 @@ class Net_SSH2
             $read = array($this->fsock);
             $write = $except = null;
 
-            if ($this->curTimeout <= 0) {
+            if (!$this->curTimeout) {
                 if ($this->keepAlive <= 0) {
                     @stream_select($read, $write, $except, null);
                 } else {
