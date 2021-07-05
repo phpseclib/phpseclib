8186db75339b0ee0cad62cc4405e7b3cede84bdc
terrafrost <terrafrost@gmail.com>
2021-07-05 08:35:39 -0500

    SFTP: get_channel_packet throws exceptions in 3.0

--- phpseclib/Net/SFTP.php
+++ phpseclib/Net/SFTP.php
@@ -2947,9 +2947,6 @@ class SFTP extends SSH2
                 $this->packet_buffer = '';
                 return false;
             }
-            if ($temp === false) {
-                return false;
-            }
             $this->packet_buffer.= $temp;
         }
         if (strlen($this->packet_buffer) < 4) {
