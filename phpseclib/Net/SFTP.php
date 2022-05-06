715bb9ff97842b4d6e5d7a8b91b309069dc60428
terrafrost <terrafrost@gmail.com>
2022-05-06 14:26:57 -0500

    SFTP: CS adjustment

--- phpseclib/Net/SFTP.php
+++ phpseclib/Net/SFTP.php
@@ -2516,7 +2516,7 @@ class SFTP extends SSH2
         // normally $entries would have at least . and .. but it might not if the directories
         // permissions didn't allow reading
         if (empty($entries)) {
-            $entries = array();
+            $entries = [];
         }
 
         unset($entries['.'], $entries['..']);
