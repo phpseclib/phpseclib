c4ec1ea0697a0dbc4b9d96e405757f823129e9be
terrafrost <terrafrost@gmail.com>
2022-09-13 23:58:14 -0500

    Random: fix fallback on PHP 8.1+

--- phpseclib/Crypt/Random.php
+++ phpseclib/Crypt/Random.php
@@ -159,7 +159,10 @@ if (!function_exists('crypt_random_string')) {
                 (isset($_POST) ? phpseclib_safe_serialize($_POST) : '') .
                 (isset($_GET) ? phpseclib_safe_serialize($_GET) : '') .
                 (isset($_COOKIE) ? phpseclib_safe_serialize($_COOKIE) : '') .
-                phpseclib_safe_serialize($GLOBALS) .
+                // as of PHP 8.1 $GLOBALS cann't be accessed by reference, which eliminates
+                // the need for phpseclib_safe_serialize. see https://wiki.php.net/rfc/restrict_globals_usage
+                // for more info
+                (version_compare(PHP_VERSION, '8.1.0', '>=') ? serialize($GLOBALS) : phpseclib_safe_serialize($GLOBALS)) .
                 phpseclib_safe_serialize($_SESSION) .
                 phpseclib_safe_serialize($_OLD_SESSION)
             ));
