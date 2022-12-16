d92ac15f3c7772441c4a5faa90a9a9701cad2c4e
Simon Podlipsky <simon@podlipsky.net>
2022-12-16 16:09:22 +0100

    fix: drop use of "self" in callable as it's deprecated since php 8.2

--- phpseclib/Math/BigInteger/Engines/PHP/Reductions/EvalBarrett.php
+++ phpseclib/Math/BigInteger/Engines/PHP/Reductions/EvalBarrett.php
@@ -105,7 +105,7 @@ abstract class EvalBarrett extends Base
                 $rhs = new ' . $class . '();
                 $lhs->value = $n;
                 $rhs->value = [' .
-                implode(',', array_map('self::float2string', $m)) . '];
+                implode(',', array_map(self::class . '::float2string', $m)) . '];
                 list(, $temp) = $lhs->divide($rhs);
                 return $temp->value;
             }
