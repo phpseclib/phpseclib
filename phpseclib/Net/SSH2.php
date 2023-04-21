79b6f96870fcce07907e6b411a2c690496b5eb4c
terrafrost <terrafrost@php.net>
2023-04-21 15:39:01 -0500

    SSH2: CS adjustment

--- phpseclib/Net/SSH2.php
+++ phpseclib/Net/SSH2.php
@@ -4266,7 +4266,9 @@ class SSH2
 
         if (strlen($packet) != $sent) {
             $this->bitmap = 0;
-            $message = $sent === false ? ('Unable to write ' . strlen($packet) . ' bytes') : ("Only $sent of " . strlen($packet) . " bytes were sent");
+            $message = $sent === false ?
+                'Unable to write ' . strlen($packet) . ' bytes' :
+                "Only $sent of " . strlen($packet) . " bytes were sent";
             throw new \RuntimeException($message);
         }
     }
