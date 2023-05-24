841267aafa5092a1c9ca3de8a5f347f61777e300
Léon Melis <leon@leonmelis.nl>
2023-05-24 17:48:43 +0200

    X509::getChain() should always return array of X509 objects
    Due to an early exit optimization, X509::getChain() could return currentCert as an array, instead of X509

--- phpseclib/File/X509.php
+++ phpseclib/File/X509.php
@@ -164,7 +164,7 @@ class X509
      *
      * @var array
      */
-    private $CAs;
+    private $CAs = [];
 
     /**
      * The currently loaded certificate
@@ -2029,9 +2029,6 @@ class X509
 
         if (!is_array($this->currentCert) || !isset($this->currentCert['tbsCertificate'])) {
             return false;
-        }
-        if (empty($this->CAs)) {
-            return $chain;
         }
         while (true) {
             $currentCert = $chain[count($chain) - 1];
