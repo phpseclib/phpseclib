585e0e44f487363326ff0900bccf4a75d0d5c70b
terrafrost <terrafrost@php.net>
2023-08-24 20:20:11 -0500

    ASN1: fix string conversion code for 32-bit PHP installs

--- phpseclib/File/ASN1.php
+++ phpseclib/File/ASN1.php
@@ -1592,7 +1592,7 @@ class File_ASN1
                         return false;
                     }
                     break;
-                case ($c & 0x80000000) != 0:
+                case ($c & (PHP_INT_SIZE == 8 ? 0x80000000 : (1 << 31))) != 0:
                     return false;
                 case $c >= 0x04000000:
                     $v .= chr(0x80 | ($c & 0x3F));
