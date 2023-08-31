615d6cfc7cf40d61bdaa9653c6a4118c6be3cf06
terrafrost <terrafrost@php.net>
2023-08-31 09:28:16 -0500

    BigInteger: fix more PHP32-bit errors

--- phpseclib/Math/BigInteger/Engines/PHP32.php
+++ phpseclib/Math/BigInteger/Engines/PHP32.php
@@ -80,10 +80,10 @@ class PHP32 extends PHP
             }
             $step = count($vals) & 3;
             if ($step) {
-                $digit = floor($digit / pow(2, 2 * $step));
+                $digit = (int) floor($digit / pow(2, 2 * $step));
             }
             if ($step != 3) {
-                $digit &= static::MAX_DIGIT;
+                $digit = (int) fmod($digit, static::BASE_FULL);
                 $i++;
             }
             $vals[] = $digit;
