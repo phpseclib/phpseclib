46602823ccbfd266709e65284647d7ca33511775
terrafrost <terrafrost@php.net>
2023-10-21 07:21:42 -0500

    CS adjustment

--- tests/Functional/Net/SFTPUserStoryTest.php
+++ tests/Functional/Net/SFTPUserStoryTest.php
@@ -807,7 +807,7 @@ class SFTPUserStoryTest extends PhpseclibFunctionalTestCase
     public function testCallableGetWithLength($sftp)
     {
         $sftp->put('test.txt', 'zzzzz');
-        $sftp->get('test.txt', function($data) {
+        $sftp->get('test.txt', function ($data) {
         }, 0, 1);
         $this->assertTrue(true);
     }
