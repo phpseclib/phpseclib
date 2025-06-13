e412112347880571eab384029c29248f46f2aa32
terrafrost <terrafrost@php.net>
2025-06-13 11:46:15 -0500

    RSA: fix for keys with negative modulos

--- phpseclib/Crypt/RSA/Formats/Keys/PKCS1.php
+++ phpseclib/Crypt/RSA/Formats/Keys/PKCS1.php
@@ -96,6 +96,20 @@ abstract class PKCS1 extends Progenitor
             $components['isPublicKey'] = true;
         }
 
+        $components = $components + $key;
+        foreach ($components as &$val) {
+            if ($val instanceof BigInteger) {
+                $val = self::makePositive($val);
+            }
+            if (is_array($val)) {
+                foreach ($val as &$subval) {
+                    if ($subval instanceof BigInteger) {
+                        $subval = self::makePositive($subval);
+                    }
+                }
+            }
+        }
+
         return $components + $key;
     }
 
@@ -156,5 +170,18 @@ abstract class PKCS1 extends Progenitor
         $key = ASN1::encodeDER($key, Maps\RSAPublicKey::MAP);
 
         return self::wrapPublicKey($key, 'RSA');
+    }
+
+    /**
+     * Negative numbers make no sense in RSA so convert them to positive
+     *
+     * @param BigInteger $x
+     * @return string
+     */
+    private static function makePositive(BigInteger $x)
+    {
+        return $x->isNegative() ?
+            new BigInteger($x->toBytes(true), 256) :
+            $x;
     }
 }
