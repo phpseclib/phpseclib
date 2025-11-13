c043719a7481deb07bf632247ce05573b5abdd8e
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2025-11-13 14:37:10 -0300

    fix: consider already integer value
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- phpseclib/Crypt/RSA/Formats/Keys/PSS.php
+++ phpseclib/Crypt/RSA/Formats/Keys/PSS.php
@@ -142,7 +142,11 @@ abstract class PSS extends Progenitor
         $result['hash'] = str_replace('id-', '', $params['hashAlgorithm']['algorithm']);
         $result['MGFHash'] = str_replace('id-', '', $params['maskGenAlgorithm']['parameters']['algorithm']);
         if (isset($params['saltLength'])) {
-            $result['saltLength'] = (int) $params['saltLength']->toString();
+            if (is_int($params['saltLength'])) {
+                $result['saltLength'] = $params['saltLength'];
+            } else {
+                $result['saltLength'] = (int) $params['saltLength']->toString();
+            }
         }
 
         if (isset($key['meta'])) {
