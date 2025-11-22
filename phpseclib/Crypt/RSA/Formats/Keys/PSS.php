d2c9048f6dbcd0523421cc1ce4cafed1e7f7c567
terrafrost <terrafrost@php.net>
2025-11-21 22:48:39 -0600

    RSA/PSS: more elegant int conversion

--- phpseclib/Crypt/RSA/Formats/Keys/PSS.php
+++ phpseclib/Crypt/RSA/Formats/Keys/PSS.php
@@ -141,11 +141,7 @@ abstract class PSS extends Progenitor
         $result['hash'] = str_replace('id-', '', $params['hashAlgorithm']['algorithm']);
         $result['MGFHash'] = str_replace('id-', '', $params['maskGenAlgorithm']['parameters']['algorithm']);
         if (isset($params['saltLength'])) {
-            if (is_int($params['saltLength'])) {
-                $result['saltLength'] = $params['saltLength'];
-            } else {
-                $result['saltLength'] = (int) $params['saltLength']->toString();
-            }
+            $result['saltLength'] = (int) "$params[saltLength]";
         }
 
         if (isset($key['meta'])) {
