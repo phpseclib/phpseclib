27f0d6e28d53567640e6ace5431c10c02e267f10
Ninette <ninette.adhikari@gmail.com>
2025-12-01 11:01:51 +0100

    rector: add custom rule ShortenShaExtends

---
+++ tests/Rector/ShortenShaExtends/config/rule.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Rules\ShortenShaExtends;
+
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->rule(ShortenShaExtends::class);
+};
