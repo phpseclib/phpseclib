1ffd22a3ce2948e7342bfdef6ec0d68bd722f89e
Ninette <ninette.adhikari@gmail.com>
2025-12-01 10:59:43 +0100

    rector: initial setup

---
+++ rector.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Rules\AddVoidLifeCycleAssert;
+use Rector\Rules\RemoveClassNamePrefix;
+
+return RectorConfig::configure()
+    ->withPaths([
+        // TODO: add project directory path to run rector
+        // __DIR__ . '/tests',
+    ])
+    ->withRules([
+        AddVoidLifeCycleAssert::class,
+        RemoveClassNamePrefix::class
+    ]);
