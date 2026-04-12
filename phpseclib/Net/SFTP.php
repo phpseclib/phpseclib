f1247a2d285649c69bfdd7a8a3af44f1ba1e48ce
terrafrost <terrafrost@php.net>
2026-04-12 16:50:18 -0500

    SFTP: make all logError() parameters required

--- phpseclib/Net/SFTP.php
+++ phpseclib/Net/SFTP.php
@@ -477,7 +477,6 @@ class SFTP extends SSH2
                     }
                     [$status] = Strings::unpackSSH2('N', $response);
                     if ($status != StatusCode::OK) {
-                        $this->logError($response, $status);
                         throw new UnexpectedValueException(
                             'Expected StatusCode::OK. '
                             . ' Got ' . $status
@@ -613,12 +612,8 @@ class SFTP extends SSH2
     /**
      * Logs errors
      */
-    private function logError(string $response, int $status = -1, ?string $filename = null): void
+    private function logError(string $response, int $status, string $filename): void
     {
-        if ($status == -1) {
-            [$status] = Strings::unpackSSH2('N', $response);
-        }
-
         $error = StatusCode::getConstantNameByValue($status);
 
         if ($this->version > 2) {
